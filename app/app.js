16cb96a8a06c927bacaab3a2539cf01df0aa98b1
Virgil Mihai <virgil.mihai@emag.ro>
2018-03-29 16:01:55 +0300

    add ui layout in template files

--- app/app.js
+++ app/app.js
@@ -15,8 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Import routes and set to app
 app.use('/', require('../routes/index'));
