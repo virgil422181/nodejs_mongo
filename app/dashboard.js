3884a7e0c59241c84575c4eb332a88f04ff0a10d
Virgil Mihai <virgil.mihai@emag.ro>
2018-05-31 15:44:16 +0300

    refactor dashboard module

--- app/dashboard.js
+++ app/dashboard.js
@@ -3,10 +3,8 @@ let url = 'mongodb://son-all34427-all-dev.scm-sigma.c.emag.network:27017';
 let connect = MongoClient.connect(url);
 
 exports.data = (req, res, next) => {
-
-    connect.then((db) => {
-        return db.db("supplier-order-node").collection("request_body").find({}).toArray();
-    }).then((data)=>{
-        res.json(data);
-    }).catch(next);
+    connect
+        .then(db => db.db("supplier-order-node").collection("request_body").find({}).toArray())
+        .then(data => res.json(data))
+        .catch(next);
 };
